d1fe3ef0260a2bd39f22dd29ed724acb371ab4ec
honeyyusuf <yusufhon0@gmail.com>
2020-11-04 20:12:46 +0000

    find the description of cat with breed name

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,22 @@
 const request = require('request');
-const breedName = process.argv.slice(2);
 
 
-request('https://api.thecatapi.com/v1/breeds/search?name=' + breedName[0],(error,response,body)=>{
+const fetchBreedDescription = function(breedName,callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search/?name=${breedName}`,(error,response,body)=>{
   
-  const data = JSON.parse(body);
-  if (error) {
-    console.log("can't connet to websit");
-  } else if (breedName.length === 0) {
-    console.log('please input the breed name');
-  } else if (data.length === 0) {
-    console.log("the breed name can't be found check if you gave the write name");
-  } else {
-    console.log(data[0].description);
-  }
+    const data = JSON.parse(body);
+    if (error !== null) {
+      callback(error,null);
+    } else if (breedName.length === 0) {
+      console.log('please input the breed name');
+    } else if (data.length === 0) {
+      console.log("the breed name can't be found check if you gave the write name");
+    } else {
+      callback(error,data[0].description);
+    }
   
   
   
-});
+  });
+};
+module.exports = { fetchBreedDescription};
